517151ae481783a22044eb119a00c1f9a5e8aabf
Wesley Souza <wfsouza23@gmail.com>
2022-09-11 21:05:12 -0300

    categoria add

--- src/main/java/com/example/demo/resources/CategoriaResource.java
+++ src/main/java/com/example/demo/resources/CategoriaResource.java
@@ -1,16 +1,28 @@
 package com.example.demo.resources;
 
+import com.example.demo.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
     @RequestMapping(method= RequestMethod.GET)
-    public String listar(){
-        return "Rest está funcionando";
+    public List<Categoria> listar(){
+
+        Categoria cat1 = new Categoria(1, "informatica");
+        Categoria cat2 = new Categoria(2, "escritorio");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
     }
 
 }
