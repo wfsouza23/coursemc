f3c30e9970b9f89521dee9fcb181281f3c044cc6
Wesley Souza <wfsouza23@gmail.com>
2022-09-11 22:39:08 -0300

    service and repository categoria

--- src/main/java/com/example/demo/resources/CategoriaResource.java
+++ src/main/java/com/example/demo/resources/CategoriaResource.java
@@ -1,28 +1,25 @@
 package com.example.demo.resources;
 
 import com.example.demo.domain.Categoria;
+import com.example.demo.services.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
-    @RequestMapping(method= RequestMethod.GET)
-    public List<Categoria> listar(){
+    @Autowired
+    private CategoriaService service;
 
-        Categoria cat1 = new Categoria(1, "informatica");
-        Categoria cat2 = new Categoria(2, "escritorio");
-
-        List<Categoria> lista = new ArrayList<>();
-        lista.add(cat1);
-        lista.add(cat2);
-
-        return lista;
+    @RequestMapping(value="/{id}", method= RequestMethod.GET)
+    public ResponseEntity<Categoria> find(@PathVariable Integer id){
+        Categoria obj = service.find(id);
+        return ResponseEntity.ok().body(obj);
     }
 
 }
---
+++ src/main/java/com/example/demo/services/CategoriaService.java
@@ -0,0 +1,20 @@
+package com.example.demo.services;
+
+import com.example.demo.domain.Categoria;
+import com.example.demo.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository repo;
+
+    public Categoria find(Integer id){
+        Optional<Categoria> obj = repo.findById(id);
+        return obj.orElse(null);
+    }
+}
