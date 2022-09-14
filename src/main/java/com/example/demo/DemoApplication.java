d01717ca7e0acffe8e8215e022e37c5095624269
Wesley Souza <wfsouza23@gmail.com>
2022-09-14 11:09:05 -0300

    produto manytomany

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,15 @@
 package com.example.demo;
 
 import com.example.demo.domain.Categoria;
+import com.example.demo.domain.Produto;
 import com.example.demo.repositories.CategoriaRepository;
+import com.example.demo.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 @SpringBootApplication
@@ -14,6 +17,8 @@ public class DemoApplication implements CommandLineRunner {
 
     @Autowired
     private CategoriaRepository categoriaRepository;
+    @Autowired
+    private ProdutoRepository produtoRepository;
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
@@ -23,7 +28,19 @@ public class DemoApplication implements CommandLineRunner {
         Categoria cat1 = new Categoria(null, "informatica");
         Categoria cat2 = new Categoria(null, "Escritorio");
 
+        Produto p1 = new Produto(null, "Computador", 2000.00);
+        Produto p2 = new Produto(null, "Impressora", 800.00);
+        Produto p3 = new Produto(null, "Mouse", 80.00);
+
+        cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+        cat2.getProdutos().addAll(Arrays.asList(p2));
+
+        p1.getCategorias().addAll(Arrays.asList(cat1));
+        p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+        p3.getCategorias().addAll(Arrays.asList(cat2));
+
         categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+        produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 
     }
 }
