9a0bc7c78019f5a54da899d74189baa9c11c1277
Wesley Souza <wfsouza23@gmail.com>
2022-09-14 11:38:12 -0300

    tratamento de excecoes

--- src/main/java/com/example/demo/resources/CategoriaResource.java
+++ src/main/java/com/example/demo/resources/CategoriaResource.java
@@ -18,6 +18,7 @@ public class CategoriaResource {
 
     @RequestMapping(value="/{id}", method= RequestMethod.GET)
     public ResponseEntity<Categoria> find(@PathVariable Integer id){
+
         Categoria obj = service.find(id);
         return ResponseEntity.ok().body(obj);
     }
--- src/main/java/com/example/demo/services/CategoriaService.java
+++ src/main/java/com/example/demo/services/CategoriaService.java
@@ -2,6 +2,7 @@ package com.example.demo.services;
 
 import com.example.demo.domain.Categoria;
 import com.example.demo.repositories.CategoriaRepository;
+import com.example.demo.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,7 @@ public class CategoriaService {
 
     public Categoria find(Integer id){
         Optional<Categoria> obj = repo.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado. Id "
+                + id + ", Tipo: "+ Categoria.class.getName()));
     }
 }
