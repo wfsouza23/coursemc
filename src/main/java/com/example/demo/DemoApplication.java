5d669a1d4d28bc0e4a28bbac973d8ae8a8642665
Wesley Souza <wfsouza23@gmail.com>
2022-09-14 10:29:58 -0300

    instanciacao

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,29 @@
 package com.example.demo;
 
+import com.example.demo.domain.Categoria;
+import com.example.demo.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class DemoApplication {
+import java.util.Arrays;
 
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Categoria cat1 = new Categoria(null, "informatica");
+        Categoria cat2 = new Categoria(null, "Escritorio");
+
+        categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+
+    }
 }
