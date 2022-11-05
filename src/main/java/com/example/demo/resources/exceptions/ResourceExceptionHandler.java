fae9bb5d9388764f8685edc0439ab7f191094b0f
Wesley Souza <wfsouza23@gmail.com>
2022-11-05 18:22:46 -0300

    adicao do DELETE em Categoria

--- src/main/java/com/example/demo/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/example/demo/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.example.demo.resources.exceptions;
 
+import com.example.demo.services.exceptions.DataIntegrityException;
 import com.example.demo.services.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,5 +17,12 @@ public class ResourceExceptionHandler {
 
         StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+    }
+
+    @ExceptionHandler(DataIntegrityException.class)
+    public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request){
+
+        StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 }
--- src/main/java/com/example/demo/services/CategoriaService.java
+++ src/main/java/com/example/demo/services/CategoriaService.java
@@ -2,8 +2,10 @@ package com.example.demo.services;
 
 import com.example.demo.domain.Categoria;
 import com.example.demo.repositories.CategoriaRepository;
+import com.example.demo.services.exceptions.DataIntegrityException;
 import com.example.demo.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -29,6 +31,17 @@ public class CategoriaService {
 
         find(obj.getId());
         return repo.save(obj);
+    }
+
+    public void delete(Integer id){
+        find(id);
+        try{
+            repo.deleteById(id);
+        }
+        catch (DataIntegrityViolationException e){
+            throw new DataIntegrityException("Nao é possivel excluir uma categoria que possui produtos");
+        }
+
     }
 
 }
