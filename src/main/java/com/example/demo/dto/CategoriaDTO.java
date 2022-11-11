a39b875343bd1f8e88c610dcbcfb7de236b7436e
Wesley de Souza <wfsouza23@gmail.com>
2022-11-10 22:20:20 -0300

    adicionando bean

--- src/main/java/com/example/demo/dto/CategoriaDTO.java
+++ src/main/java/com/example/demo/dto/CategoriaDTO.java
@@ -1,13 +1,19 @@
 package com.example.demo.dto;
 
 import com.example.demo.domain.Categoria;
+import org.hibernate.validator.constraints.Length;
 
+
+import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 
 public class CategoriaDTO implements Serializable {
     private static final long serialVersionUID = 1l;
 
     private Integer id;
+    
+    @NotEmpty(message = "Preenchimento Obrigatorio")
+    @Length(min = 5, max = 80, message = "O tamanho deve ser entre 5 e 80 caracteres")
     private String nome;
 
     public CategoriaDTO() {
